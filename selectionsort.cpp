6d9e666af4e2a87820bbf5d20ff5559f65e6323b
etsubdink-m <etsubdinkmersha@gmail.com>
2022-09-08 19:08:22 -0700

    GeeksForGeeks:selection sort

---
+++ selectionsort.cpp
@@ -0,0 +1,17 @@
+class solution {
+    public:
+    int select(int arr[],int i){
+        return arr[i];
+    }
+    void selectionsort(int arr[], int n){
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                if (arr[j]<arr[i]){
+                   int temp=arr[i];
+                   arr[i]=arr[j];
+                   arr[j]=temp;
+                }
+            }
+        }
+    }
+}
